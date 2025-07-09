c2ca8b2d90159059f9bab8eab9d204fcd0da7183
Anvith shetty <161817374+Anvithshetty17@users.noreply.github.com>
2025-07-09 12:15:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -57,12 +57,11 @@ app.use("/api/messages", messageRouter)
 await connectDB()
 
 
-if (process.env.NODE_ENV !== "production") {
-    const PORT = process.env.PORT || 5000
-    server.listen(PORT, () => {
-        console.log("Server is running on port :", PORT)
-    })
-}
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log("Server is running on port :", PORT);
+});
+
 
 // Export the server for versel
 export default server
